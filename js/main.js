2a308f25aee1a1f06ca95000e9bd8017f543f189
Paola Santos <paola.santos@compasso.com.br>
2019-11-06 17:44:39 -0300

    projeto trx da capacitação

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$("a").click(function (event) {
+    event.preventDefault();
+});
+
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+});
